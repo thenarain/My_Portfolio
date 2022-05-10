5840cdeef7ba8c9f831f1b454b520cc841b5f9fe
Kartikay <kartiksrivastava19@gmail.com>
2022-05-10 13:29:43 +0100

    Update markup and styles for nav-bar and add script file

---
+++ script.js
@@ -0,0 +1,5 @@
+const mainMenu = document.querySelector('.main-menu');
+const openMenu = document.querySelector('.open-menu');
+const closeMenu = document.querySelector('.close-menu');
+const logo = document.querySelector('.logo');
+const navBar = document.querySelector('.nav-bar');
