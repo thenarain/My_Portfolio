8ebc96052219c859778013c74f958157fa0d42a9
Kartikay <kartiksrivastava19@gmail.com>
2022-05-10 13:35:28 +0100

    Add show function in script

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const openMenu = document.querySelector('.open-menu');
 const closeMenu = document.querySelector('.close-menu');
 const logo = document.querySelector('.logo');
 const navBar = document.querySelector('.nav-bar');
+
+function show() {
+    closeMenu.classList.toggle('hidden');
+    navBar.classList.add('show');
+    openMenu.classList.add('hidden');
+    mainMenu.classList.toggle('hidden');
+    logo.classList.add('hidden');
+  }
